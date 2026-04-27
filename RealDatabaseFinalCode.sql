514674c2fbcbb9367c98f318c61200a8753adb43
alexed154 <aedmon01@villanova.edu>
2026-04-27 17:56:07 -0400

    test

---
+++ RealDatabaseFinalCode.sql
@@ -0,0 +1,14 @@
+Drop Table Students;
+
+Create table Students (
+    student_id varchar(10) PRIMARY KEY NOT NULL, 
+    first_name varchar(20) NOT NULL, 
+    last_name varchar(20) NOT NULL , 
+    email varchar(20) NOT NULL UNIQUE 
+);
+
+Insert into Students
+Values('2026ABCDEF', 'Alexander', 'Edmonds', 'aedmonds');
+
+select * from Students;
+
